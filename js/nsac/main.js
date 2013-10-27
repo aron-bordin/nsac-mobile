c2578ddd9953894ec9d5e2037685efe60da419c8
Aron Bordin <aron.bordin@gmail.com>
2013-10-27 07:32:35 -0200

    Iniciando aprimoramento  dos alerts para notifications

--- js/nsac/main.js
+++ js/nsac/main.js
@@ -1,5 +1,49 @@
 app.nsac = {
-	login: function(){
+	login : function() {
+		var txtMat = $("#Mat");
+		var txtSenha = $("#pass");
+		if (txtMat.val() === "") {
+			alert("Digite o número de matrícula!");
+			return;
+		}
+		if (txtSenha.val() === "") {
+			alert("Digite a senha!");
+			return;
+		}
 		
+		Data = "txtMat=" + txtMat.val() + " &txtSenha=" + txtSenha.val() + "&inputsubmit1=+++Logar+++&pagina=login";
+		$.ajax({
+			url : "http://www.cti3.feb.unesp.br/nsac/verifica_login.php",
+			type : "POST",
+			data : Data,
+			success : function(response, status, request) {
+				console.log(response);
+				console.log(status);
+				var header = request.getAllResponseHeaders();
+				var match = header.match(/(Set-Cookie|set-cookie): (.+?);/);
+				if (match){}
+				
+				//testa se server ok
+				if(status !== "success"){
+					alert("Falha ao conectar ao servidor");
+					return;
+				}
+				
+				if(response.search("incorreto!") !== -1){
+					alert("Nome de usuário ou senha incorreto");
+					return;
+				}
+				if(response.search("bemvindo") === -1){
+					alert("Falha ao conectar ao servidor");
+					return;
+				}
+				
+				alert("Login ok!");					
+			},
+			error : function(data) {
+				alert(2);
+				$("#txt").html(data);
+			}
+		});
 	}
 };
