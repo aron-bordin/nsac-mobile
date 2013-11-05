f912bde52ed81f141a881b41721b72888eb774f6
Aron Bordin <aron.bordin@gmail.com>
2013-11-05 19:19:00 -0200

    Sobre OK

--- js/nsac/main.js
+++ js/nsac/main.js
@@ -53,7 +53,8 @@ app.nsac = {
 
 			},
 			error : function(data) {
-				//a);
+				navigator.notification.alert("Falha ao se conectar ao servidor!. Verifique sua conexão", function() {
+				}, "Falha");
 			}
 		});
 	},
@@ -63,7 +64,7 @@ app.nsac = {
 
 	abrirOcorrencias : function() {
 		var Lista = $("#list");
-		if(app.nsac.Ocorrencias.length < 2){
+		if (app.nsac.Ocorrencias.length < 2) {
 			Lista.html("<p>Você não tem nenhume ocorrência :)");
 		} else {
 			Lista.html(app.nsac.Ocorrencias);
@@ -76,7 +77,8 @@ app.nsac = {
 			type : "get",
 			success : function(data) {
 				var Lista = $("#list");
-				var Aux = data; // pra salvar as ocorrencias, ja q vem na msm pag
+				var Aux = data;
+				// pra salvar as ocorrencias, ja q vem na msm pag
 				// boletim
 				Aux = Aux.split('<div id="tabs-3">');
 				Aux = Aux[1];
@@ -103,7 +105,7 @@ app.nsac = {
 		Topo.html("<h1>NSac Mobile</h1><a class='button' style='float: rigth' id='mostrarMenu' href='javascript:void(null)'>Menu</a>");
 		$.UISheet();
 		$('.sheet').find('section').append("<ul class='list'></li>");
-		$('.sheet .list').append("<li><a class='button' onclick='app.nsac.abrirBoletim()'>Boletim</a></li><li><a class='button' onclick='app.nsac.abrirOcorrencias()'>Ocorrências</a></li><li><a class='button' href='javascript:void(null)'>Cancelar</a></li>");
+		$('.sheet .list').append("<li><a class='button' onclick='app.nsac.abrirBoletim()'>Boletim</a></li><li><a class='button' onclick='app.nsac.abrirOcorrencias()'>Ocorrências</a></li><li><a class='button' href='index.html'>Logoff</a></li><li><a class='button' onclick='app.nsac.sobre()'> Sobre </a></li><li><a class='button' href='javascript:void(null)'>Cancelar</a></li>");
 		$('.sheet .list').append('<h2 style="text-align: center; margin: 20px;">NSac Mobile</h2>');
 		$("#mostrarMenu").click(function() {
 			$.UIShowSheet();
@@ -153,5 +155,46 @@ app.nsac = {
 			$("#Mat").val(localStorage.getItem("mat"));
 			$("#pass").val(localStorage.getItem("senha"));
 		}
+	},
+
+	sobre : function() {
+		var Lista = $("#list");
+		var Html = "";
+		Html += "<div align='center' ><li><h1> NSac Mobile </h1></li>";
+		Html += "<li><h2> Desenvolvimento </h2>";
+		Html += "<p> Aron Barreira Bordin </p>";
+		Html += "<p><a href='mailto:aron.bordin@gmail.com'>aron.bordin@gmail.com</a></p>";
+		Html += "<p> Versão 1.0 - Aluno </p>";
+		Html += "<p> Última atualização: 06/09/2013 </p>";
+		Html += "</li>";
+		Html += "<li> <h2> Licença </h2> ";
+		Html += "<p> Projeto de código aberto sobre licença GPLv3.0</p>";
+		Html += "<a href='http://www.gnu.org/licenses/gpl.html'> Ler a licença </a><li>";
+		Html += "<li> <h2> Contribuição </h2>";
+		Html += "<p> Por ser um projeto de código aberto, todo o código esta disponível para Download e estudo.<p>";
+		Html += "<p> Caso se interesse em colaborar com o desenvolvimento(programando, sugestões, testar versões betas, informar bugs, etc), entre em contato via e-mail, Facebook, Skype, Telepatia, ...<br> E assim por diante.</p>";
+		Html += "<p> Como dito logo acima, você pode fazer o Download do código e se divertir acessando: <a href='https://github.com/aron-bordin/nsac-mobile'>https://github.com/aron-bordin/nsac-mobile</a></p>";
+		Html += "<p> Nos vemos pela rede. T++</p></li></div>";
+
+		Lista.html(Html);
+	},
+
+	confirmarDados : function() {
+		$.ajax({
+			url : "http://www.cti3.feb.unesp.br/nsac/index.php?pag=confirma_dados",
+			type : "get",
+			success : function(data) {
+
+				data = data.split('<table style="text-align:left;" cellspacing="10">');
+				data = data[1];
+				data = data.split("</table>");
+				data = data[0];
+				$("#list").html(data);
+				console.log(data);
+			},
+			error : function() {
+				console.error("Erro");
+			}
+		});
 	}
 };
